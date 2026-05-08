76298547449a2db866041f704e04e32ade14abca
daimonia <her@chloero.se>
2026-05-08 14:47:42 -0400

    feat: add glyph art, play scene

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -2,16 +2,24 @@ extends Camera2D
 
 
 @export var max_speed: float = 1.0
-@export var drag: float = 0.9
+@export var zoom_speed: float = 0.1
 
-var speed: Vector2 = Vector2(0, 0)
+var velocity: Vector2 = Vector2(0, 0)
+var zoom_velocity: Vector2 = Vector2(0, 0)
 
 
 func _process(delta: float) -> void:
-    position += speed * delta
-    # speed *= drag
+    position += velocity * delta
+    zoom += zoom_velocity * delta
 
 
-func _input(_event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
     var input_velocity = Input.get_vector("CameraLeft", "CameraRight", "CameraUp", "CameraDown")
-    speed = max_speed * input_velocity
+    velocity = max_speed * input_velocity
+
+    if event.is_action_pressed("ZoomIn"):
+        zoom_velocity = Vector2(zoom_speed, zoom_speed)
+    elif event.is_action_pressed("ZoomOut"):
+        zoom_velocity = -Vector2(zoom_speed, zoom_speed)
+    else:
+        zoom_velocity = Vector2.ZERO
