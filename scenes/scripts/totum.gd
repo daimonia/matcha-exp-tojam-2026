431a067a7b8c61ca0c06970b2cc54601e4361e77
Kaiya Morgulis <kaiya.kfir@gmail.com>
2026-05-08 15:56:06 -0400

    IT SPINS

--- scenes/scripts/totum.gd
+++ scenes/scripts/totum.gd
@@ -6,17 +6,17 @@ extends Node2D
 
 
 func _ready() -> void:
-    _update_glyph()
+	_update_glyph()
 
 
 func _update_glyph():
-    for glyph in glyphs:
-        var sprite = Sprite2D.new()
-        sprite.texture = glyph.texture
-        add_child(sprite)
+	for glyph in glyphs:
+		var sprite = Sprite2D.new()
+		sprite.texture = glyph.texture
+		add_child(sprite)
 
 
 func _process(_delta: float) -> void:
-    if Engine.is_editor_hint():
-        # running as a tool script, so don't do anything wild
-        return
+	if Engine.is_editor_hint():
+		# running as a tool script, so don't do anything wild
+		return
