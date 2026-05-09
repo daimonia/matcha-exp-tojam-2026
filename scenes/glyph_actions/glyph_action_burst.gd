d7630b81bdff88c139eab365c04b01282465bbd0
Kaiya Morgulis <kaiya.kfir@gmail.com>
2026-05-09 18:02:50 -0400

    somewhatFunctioningAttacks

--- scenes/glyph_actions/glyph_action_burst.gd
+++ scenes/glyph_actions/glyph_action_burst.gd
@@ -20,3 +20,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
+
+
+func _on_timer_timeout() -> void:
+    pass # Replace with function body.
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+    pass # Replace with function body.
--- scenes/glyph_actions/glyph_action_compass.gd
+++ scenes/glyph_actions/glyph_action_compass.gd
@@ -16,14 +16,14 @@ func _ready() -> void:
         var cell = current_layer.get_cell_at_global_position(global_position)
         #check if there's a cell where the totum is and if so, deal damage
         if cell:
-            #cell.take_damage(1000)
+            #cell.get_cell_at_local_position(cell).take_damage(1000)
             #deal damage to the layer below
-            current_layer.layer_below.get_cell_at_global_position(global_position).take_damage(10)
+            current_layer.layer_below.get_cell_at_position(cell.coords).take_damage(1000)
             #deal damage to the cardinal directions on same layer
             for direction in damage_directions:
-                var temp = cell.get_cell_at_position(cell.coords + direction)
+                var temp = current_layer.get_cell_at_position(cell.coords + direction)
                 if temp:
-                    temp.take_damage(10)
+                    temp.take_damage(1000)
     else:
         push_warning('%s: no current layer specified' % name)
 
