ff6358423998b64280cd925c159612733a7f3d80
Kaiya Morgulis <kaiya.kfir@gmail.com>
2026-05-09 17:34:06 -0400

    yeeaayyyyy

---
+++ scenes/glyph_actions/glyph_action_burst.gd
@@ -0,0 +1,22 @@
+extends BaseGlyphAction
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    sprite.play()
+    audio_player.play()
+    
+    if current_layer:
+        var cell = current_layer.get_cell_at_global_position(global_position)
+
+        if cell:
+            cell.take_damage(1000)
+    else:
+        push_warning('%s: no current layer specified' % name)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
--- scenes/glyph_actions/glyph_action_compass.gd
+++ scenes/glyph_actions/glyph_action_compass.gd
@@ -4,16 +4,26 @@ extends BaseGlyphAction
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+
+var damage_directions: Array[Vector2] = [Vector2(0,1),Vector2(0,-1),Vector2(-1,0),Vector2(1,0)]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     sprite.play()
     audio_player.play()
 
-    if current_layer:
+    if current_layer: #passed by the totum when it attacks
         var cell = current_layer.get_cell_at_global_position(global_position)
-
+        #check if there's a cell where the totum is and if so, deal damage
         if cell:
-            cell.take_damage(1000)
+            #cell.take_damage(1000)
+            #deal damage to the layer below
+            current_layer.layer_below.get_cell_at_global_position(global_position).take_damage(10)
+            #deal damage to the cardinal directions on same layer
+            for direction in damage_directions:
+                var temp = cell.get_cell_at_position(cell.coords + direction)
+                if temp:
+                    temp.take_damage(10)
     else:
         push_warning('%s: no current layer specified' % name)
 
