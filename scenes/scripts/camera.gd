10759e7c72bc49ebac48e8b6b3c0285a97509185
daimonia <her@chloero.se>
2026-05-10 16:56:34 -0400

    feat: da map is done :=

--- scenes/scripts/camera.gd
+++ scenes/scripts/camera.gd
@@ -4,24 +4,21 @@ extends Camera2D
 @export var max_speed: float = 1.0
 @export var zoom_speed: float = 0.5
 
+@export var max_zoom: float = 4
+@export var min_zoom: float = 0.1
+
 var velocity: Vector2 = Vector2(0, 0)
-var zoom_velocity: Vector2 = Vector2(0, 0)
 
 
 func _process(delta: float) -> void:
     position += velocity * delta
-    zoom += zoom_velocity * delta
+
+    if Input.is_action_pressed("ZoomIn"):
+        zoom += Vector2(zoom_speed, zoom_speed) * delta
+    elif Input.is_action_pressed("ZoomOut"):
+        zoom -= Vector2(zoom_speed, zoom_speed) * delta
 
 
 func _input(event: InputEvent) -> void:
     var input_velocity = Input.get_vector("CameraLeft", "CameraRight", "CameraUp", "CameraDown")
     velocity = max_speed * input_velocity
-
-    if event.is_action_pressed("ZoomIn"):
-        zoom_velocity = Vector2(zoom_speed, zoom_speed)
-    elif event.is_action_pressed("ZoomOut"):
-        zoom_velocity = Vector2(-zoom_speed, -zoom_speed)
-    else:
-        zoom_velocity = Vector2.ZERO
-    #this is a test of zoom velocity
-    #zoom += zoom_velocity
