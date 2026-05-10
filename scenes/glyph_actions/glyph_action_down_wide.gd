5c7316547832b5464e65488f31c463f8b3360cd7
Kaiya Morgulis <kaiya.kfir@gmail.com>
2026-05-09 20:41:57 -0400

    downWide

---
+++ scenes/glyph_actions/glyph_action_down_wide.gd
@@ -0,0 +1,42 @@
+extends BaseGlyphAction
+
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+var rng = RandomNumberGenerator.new()
+#choose a number of tiles to dig down
+var times: int = 2
+
+#set up an array of the 4 cardinal directions
+var damage_directions: Array[Vector2] = [Vector2(0,1),Vector2(0,-1),Vector2(-1,0),Vector2(1,0)]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    sprite.play()
+    audio_player.play()
+
+    if current_layer: #passed by the totum when it attacks
+        var cell = current_layer.get_cell_at_global_position(global_position)
+        #check if there's a cell where the totum is and if so, deal damage
+        if cell:
+            #deal damage to the same tile on each layer down for each time generated
+            var temp = current_layer.get_cell_at_position(cell.coords)
+            for time in times:
+                if temp:
+                    temp.take_damage(1000)
+                    for direction in damage_directions:
+                        var tempDirection = current_layer.get_cell_at_position(cell.coords + direction)
+                        tempDirection.take_damage(1000)
+                    temp = current_layer.layer_below.get_cell_at_position(cell.coords)
+    else:
+        push_warning('%s: no current layer specified' % name)
+
+
+func _on_timer_timeout() -> void:
+    attack_ended.emit()
+    queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+    sprite.hide()
