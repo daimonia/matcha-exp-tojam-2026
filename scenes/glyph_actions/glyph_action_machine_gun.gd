1649e4e926fa2936a6a4167018de1a25834aca65
Kaiya Morgulis <kaiya.kfir@gmail.com>
2026-05-09 22:15:39 -0400

    Machine Gun and Ravine

---
+++ scenes/glyph_actions/glyph_action_machine_gun.gd
@@ -0,0 +1,41 @@
+extends BaseGlyphAction
+
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+#cardinal directions
+var damage_directions: Array[Vector2] = [Vector2(0,1),Vector2(0,-1),Vector2(-1,0),Vector2(1,0)]
+
+var rng = RandomNumberGenerator.new()
+#number of bullets
+var times = rng.rangei(1,3)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    sprite.play()
+    audio_player.play()
+
+    if current_layer: #passed by the totum when it attacks
+        var cell = current_layer.get_cell_at_global_position(global_position)
+        #check if there's a cell where the totum is and if so, deal damage
+        if cell:
+            current_layer.get_cell_at_position(cell.coords).take_damage(1000)
+            
+            #deal damage to the cardinal directions on same layer
+            for direction in damage_directions:
+                for i in times:
+                    var temp = current_layer.get_cell_at_position(cell.coords + direction*i)
+                    if temp:
+                        temp.take_damage(1000)
+    else:
+        push_warning('%s: no current layer specified' % name)
+
+
+func _on_timer_timeout() -> void:
+    attack_ended.emit()
+    queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+    sprite.hide()
---
+++ scenes/glyph_actions/glyph_action_ravine.gd
@@ -0,0 +1,46 @@
+extends BaseGlyphAction
+
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+#cardinal axes
+var damage_directions: Array[Vector2] = [Vector2(1,0),Vector2(0,1)]
+
+var rng = RandomNumberGenerator.new()
+#choose a direction
+var direction: int = rng.randi_range(0,1)
+#length of the ravine
+var length: int = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    sprite.play()
+    audio_player.play()
+
+    if current_layer: #passed by the totum when it attacks
+        var cell = current_layer.get_cell_at_global_position(global_position)
+        #check if there's a cell where the totum is and if so, deal damage
+        if cell:
+            #current_layer.get_cell_at_position(cell.coords).take_damage(1000)
+            
+            #deal damage to the cardinal directions on same layer
+            #for direction in damage_directions:
+                for i in length:
+                    var temp = current_layer.get_cell_at_position(cell.coords + direction*(i-2))
+                    if temp:
+                        temp.take_damage(1000)
+                        temp = current_layer.layer_below.get_cell_at_position(cell.coords + direction*(i-2))
+                        if temp:
+                            temp.take_damage(1000)
+    else:
+        push_warning('%s: no current layer specified' % name)
+
+
+func _on_timer_timeout() -> void:
+    attack_ended.emit()
+    queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+    sprite.hide()
