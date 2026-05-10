b8efc3256d2b5f5ce68e55dbf8e222f447d06bd7
daimonia <her@chloero.se>
2026-05-10 13:23:30 -0400

    feat: rewards fanfare, quit the game

--- scenes/scripts/play.gd
+++ scenes/scripts/play.gd
@@ -17,6 +17,14 @@ func _process(_delta: float) -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event: InputEvent) -> void:
+    if event.is_action("EndGame") and event.is_pressed() and not event.is_echo():
+        if $CanvasLayer/GameEnd.visible:
+            # if the menu is already up, pressing esc again quits the game
+            get_tree().quit()
+
+        end_game(false)
+        return
+
     if event is InputEventKey:
         match event.physical_keycode:
             49:
